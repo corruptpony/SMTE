d6577b957bdb181fcbbf22d95e67892e5b881990
corruptpony <tpj_ackermans@hotmail.com>
2015-03-13 12:31:37 +0100

    created first project

---
+++ Week 1 - Tim en Lars/Week 1 - Tim en Lars/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Week 1 - Tim en Lars
+//
+//  Created by Fhict on 13/03/15.
+//  Copyright (c) 2015 Fhict. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
